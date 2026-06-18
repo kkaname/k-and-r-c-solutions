e76a3c13d6a8ceea1bd897f2e01177a04e9b1a52
kkaname <ayushhegde2005@gmail.com>
2026-06-19 02:15:24 +0530

    solution for exercise 1.04

---
+++ chapter_1/exercise_1-04/cels_fahr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 200;
+    step = 10;
+
+    printf("Celsius\t\tFahrenheit\n");
+    printf("-----------------------\n");
+
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = (9.0/5.0) * celsius + 32;
+        printf("%6.2f\t\t%6.2f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+    printf("-----------------------\n");
+
+    return 0;
+}
+
