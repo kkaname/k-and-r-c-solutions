c4d86d37f727a474532c669707632de3da6cd8ea
kkaname <ayushhegde2005@gmail.com>
2026-06-20 02:44:20 +0530

    solution for exercise 1.09

---
+++ chapter_1/exercise_1-09/replace_blanks.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int c, last_c;
+    last_c = '\0';
+
+    while ((c = getchar()) != EOF) {
+        if (!(c == ' ' && last_c == ' ')) {
+            putchar(c);
+        }
+        last_c = c; // saves current character to detect consecutive spaces
+    }
+    // Note: only multiple spaces are collapsed into one, not tabs.
+
+    return 0;
+}
+
