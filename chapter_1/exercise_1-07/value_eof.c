b9020e5728ad1da8930947aa411ced8965639525
kkaname <ayushhegde2005@gmail.com>
2026-06-20 02:32:22 +0530

    solution for exercise 1.07

---
+++ chapter_1/exercise_1-07/value_eof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// EOF if defined in stdio.h
+
+int main(void) {
+    printf("Value of EOF: %d.\n", EOF);;
+    // EOF is system/compiler dependent, but usually is -1.
+
+    return 0;
+}
+
