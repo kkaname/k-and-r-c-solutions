4667d8776e3ae6bba9026d60a7e684285ff6566b
kkaname <ayushhegde2005@gmail.com>
2026-06-20 13:24:03 +0530

    solution for exercise 1.15

---
+++ chapter_1/exercise_1-15/temp_function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+void fahr_to_celsius(float fahr) {
+    float celsius;
+    celsius = (5.0/9.0) * (fahr - 32);
+    printf("%3.0f\t\t%.2f\n", fahr, celsius);
+    return;
+}
+
+
+int main(void) {
+    float fahr;
+
+    printf("Fahrenheit\tCelsius\n-------------------------\n");
+    fahr = LOWER;
+    while (fahr <= UPPER) {
+        fahr_to_celsius(fahr);
+        fahr += STEP;
+    }
+
+    return 0;
+}
