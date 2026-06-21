5e6d357303522594b7e80cb654b98db770880958
kkaname <ayushhegde2005@gmail.com>
2026-06-21 13:44:59 +0530

    fix: update indentation to 2-space wide

--- chapter_1/exercise_1-08/count_blank_char.c
+++ chapter_1/exercise_1-08/count_blank_char.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
-    int num_blank, num_tab, num_newline, c;
-    num_blank = num_tab = num_newline = 0;
+  int num_blank, num_tab, num_newline, c;
+  num_blank = num_tab = num_newline = 0;
 
-    while ((c = getchar()) != EOF) {
-        switch (c) {
-            case '\n':
-                ++num_newline;
-                break;
-            case '\t':
-                ++num_tab;
-                break;
-            case ' ':
-                ++num_blank;
-                break;
-        }
+  while ((c = getchar()) != EOF) {
+    switch (c) {
+      case '\n':
+        ++num_newline;
+        break;
+      case '\t':
+        ++num_tab;
+        break;
+      case ' ':
+        ++num_blank;
+        break;
     }
-    /*
-     * The reason I have prefered switch statement is that
-     * it is faster then if - else if ladder and better readability.
-     *
-     * You can also use if-else if ladder as follows:
-     *
-     * while ((c = getchar()) != EOF) {
-     *   if (c == '\n') {
-     *       ++num_newline;
-     *   }
-     *   else if (c == '\t') {
-     *       ++num_tab;
-     *   }
-     *   else if (c == ' ') {
-     *       ++num_blank;
-     *   }
-     * }
-     */
+  }
+  /*
+   * The reason I have prefered switch statement is that
+   * it is faster then if - else if ladder and better readability.
+   *
+   * You can also use if-else if ladder as follows:
+   *
+   * while ((c = getchar()) != EOF) {
+   *   if (c == '\n') {
+   *       ++num_newline;
+   *   }
+   *   else if (c == '\t') {
+   *       ++num_tab;
+   *   }
+   *   else if (c == ' ') {
+   *       ++num_blank;
+   *   }
+   * }
+   */
 
-    printf("blanks  :   %d\n", num_blank);
-    printf("tabs    :   %d\n", num_tab);
-    printf("newlines:   %d\n", num_newline);
+  printf("blanks  :   %d\n", num_blank);
+  printf("tabs    :   %d\n", num_tab);
+  printf("newlines:   %d\n", num_newline);
 
-    return 0;
+  return 0;
 }
-
