b945f1685a687b7b57b97a32d3780e0615e7f86c
kkaname <ayushhegde2005@gmail.com>
2026-06-21 13:38:16 +0530

    fix: updated the indentation to 2-space wide

--- chapter_1/exercise_1-03/fahr_cels.c
+++ chapter_1/exercise_1-03/fahr_cels.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    float fahr, celsius;
-    int lower, upper, step;
+  float fahr, celsius;
+  int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+  lower = 0;
+  upper = 300;
+  step = 20;
 
-    printf("Fahrenheit\tCelsius\n");
-    printf("-----------------------\n");
+  printf("Fahrenheit\tCelsius\n");
+  printf("-----------------------\n");
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr - 32);
-        printf("%6.2f\t\t%6.2f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
-    printf("-----------------------\n");
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr - 32);
+    printf("%6.2f\t\t%6.2f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+  printf("-----------------------\n");
 
-    return 0;
+  return 0;
 }
-
