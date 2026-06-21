28db231b51cadc22ef6d19181077bff01ba1e323
kkaname <ayushhegde2005@gmail.com>
2026-06-21 13:42:47 +0530

    fix: update indentation to 2-space wide

--- chapter_1/exercise_1-07/value_eof.c
+++ chapter_1/exercise_1-07/value_eof.c
@@ -2,9 +2,8 @@
 // EOF if defined in stdio.h
 
 int main(void) {
-    printf("Value of EOF: %d.\n", EOF);;
-    // EOF is system/compiler dependent, but usually is -1.
+  printf("Value of EOF: %d.\n", EOF);
+  // EOF is system/compiler dependent, but usually is -1.
 
-    return 0;
+  return 0;
 }
-
