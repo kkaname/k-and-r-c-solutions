56e9c6bc13c4e848d658ea907dd95cf7ea809a87
kkaname <ayushhegde2005@gmail.com>
2026-06-25 01:07:13 +0530

    refactor: removed while loop comment, changed limit variables type and improved formate of the output table

--- chapter_1/exercise_1-03/fahr_cels.c
+++ chapter_1/exercise_1-03/fahr_cels.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
-#define SEPARATOR "-----------------------"
+#define SEPARATOR "---------------------"
 
 int main(void) {
   int fahr;
   float celsius;
-  int lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+  const int lower = 0;
+  const int upper = 300;
+  const int step = 20;
 
-  printf("Fahrenheit\tCelsius\n");
+  printf("%10s %10s\n", "Fahrenheit", "Celsius");
   printf(SEPARATOR "\n");
 
   for (fahr = lower; fahr <= upper; fahr += step) {
     celsius = (5.0/9.0) * (fahr - 32);
-    printf("%6d\t\t%6.2f\n", fahr, celsius);
+    printf("%10d %10.2f\n", fahr, celsius);
   }
-  /* K and R's Version of code:
-   * fahr = lower;
-   * while (fahr <= upper) {
-   *   celsius = (5.0/9.0) * (fahr - 32);
-   *   printf("%6.2f\t\t%6.2f\n", fahr, celsius);
-   *   fahr = fahr + step;
-   * }
-   */
 
   printf(SEPARATOR "\n");
 
