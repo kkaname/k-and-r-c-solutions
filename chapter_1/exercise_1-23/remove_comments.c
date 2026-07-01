8b9ad85c308deca2f9e97687eb84afaa6bb6a33a
kkaname <ayushhegde2005@gmail.com>
2026-07-01 12:36:42 +0530

    refactor: improve the code to handle all edge cases and corrected the errors in the initial logic

--- chapter_1/exercise_1-23/remove_comments.c
+++ chapter_1/exercise_1-23/remove_comments.c
@@ -2,19 +2,18 @@
 
 #define MAXLEN 1000
 
-#define TRUE (1 == 1)
-#define FALSE !TRUE
+enum states{normal, character, string, multi_line_comment, single_line_comment};
 
 int get_str(char str[]);
-void removeComments(char str[], char no_comm_str[]);
+void removeComments(char str[], int len, char no_comment_str[]);
 
 int main(void) {
-  char str[MAXLEN], no_comm_str[MAXLEN];
+  char str[MAXLEN], no_comment_str[MAXLEN];
 
-  get_str(str);
-  removeComments(str, no_comm_str);
+  int len = get_str(str);
+  removeComments(str , len, no_comment_str);
 
-  printf("%s\n", no_comm_str);
+  printf("%s\n", no_comment_str);
 
   return 0;
 }
@@ -25,52 +24,74 @@ int get_str(char str[]) {
   for (i = 0; i < MAXLEN - 1 && (c = getchar()) != EOF; i++) {
     str[i] = c;
   }
-
   str[i] = '\0';
-  putchar('\n');
 
   return i;
 }
 
-void removeComments(char str[], char no_comm_str[]) {
-  unsigned int i, j;
-  int in_multiline_comm, in_singleline_comm;
-
-  in_multiline_comm = FALSE;
-  in_singleline_comm = FALSE;
+void removeComments(char str[], int len, char no_comment_str[]) {
+  int i, j;
+  enum states state;
+  state = normal;
 
   for (i = 0, j = 0; str[i] != '\0'; i++) {
-    if (str[i] == '/' && str[i + 1] == '*') {
-      in_multiline_comm = TRUE;
-      i += 2;
+    if (str[i] == '/' && i + 1 < len - 1 && str[i + 1] == '*' && state != string && state != character) {
+      state = multi_line_comment;
+      ++i;
     }
-    if (str[i] == '/' && str[i + 1] == '/') {
-      in_singleline_comm = TRUE;
-      i += 2;
+    else if (str[i] == '/' && i + 1 < len - 1 && str[i + 1] == '/' && state != string && state != character) {
+      state = single_line_comment;
+      ++i;
     }
-
-    if (in_multiline_comm) {    //here we check whether the iterator is inside a comment or not, if not
-      if (str[i] == '*' && str[i + 1] == '/') {
-        in_multiline_comm = FALSE;
+    else if (str[i] == '*' && i + 2 < len - 1 && str[i + 1] == '/') {
+      if (state == multi_line_comment) {
         i += 2;
-        no_comm_str[j++] = '\n';
-      }
-      else {
-        continue;
+        // increment 1 by two cause it skips str[i] = '*' and str[i + 1] = '/'
+        // and i indexs to next element after "*/" and at the same time the state is
+        // made to normal, due to which at the end of the loop, whatever comes after "*/"
+        // goes to no_comment_str through -> no_comment_str[j++] = str[i]
+        state = normal;
       }
     }
-    else if (in_singleline_comm) {
-      if (str[i] == '\n'){
-        in_singleline_comm = FALSE;
+    else if (str[i] == '\n' && state == single_line_comment) {
+      state = normal;
+    }
+    else if (str[i] == '\"' && state != multi_line_comment && state != single_line_comment) {
+      if (state == string) {
+        state = normal;
       }
       else {
+        state = string;
+      }
+    }
+    else if (str[i] == '\'' && state != multi_line_comment && state != single_line_comment) {
+      if (state == normal) {
+        state = character;
+      }
+      else if (state == character) {
+        state = normal;
+      }
+    }
+   else if (str[i] == '\\' && (str[i + 1] == '\'' || str[i + 1] == '\"')) {
+      if (state == string || state == character) {
+        no_comment_str[j++] = '\\';
+        ++i;
+        if (str[i] == '\'') {
+          no_comment_str[j++] = '\'';
+        }
+        else {
+          no_comment_str[j++] = '\"';
+        }
         continue;
       }
     }
 
-    if (!in_multiline_comm && !in_singleline_comm) {
-      no_comm_str[j++] = str[i];
-    }
+   if (state == multi_line_comment || state == single_line_comment) {
+     continue;
+   }
+   else {
+     no_comment_str[j++] = str[i];
+   }
   }
-  no_comm_str[j] = '\0';
+  no_comment_str[j] = '\0';
 }
